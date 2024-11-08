f49da3cacf5f1163abb516ae4d59fb37c768fe97
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-11-08 09:58:27 -0600

    COmpleted

---
+++ src/app/user/sign-in/sign-in.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IUserCredentials } from '../user.model';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'bot-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css'],
+})
+export class SignInComponent {
+  credentials: IUserCredentials = { email: '', password: '' };
+  signInError: boolean = false;
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  //call sign in method in userService, handlw error by tapping into
+  //error event. Now modify sign-in template
+  signIn() {
+    this.signInError = false;
+    this.userService.signIn(this.credentials).subscribe({
+      next: () => this.router.navigate(['/catalog']),
+      error: () => (this.signInError = true)
+    });
+  }
+
+}
